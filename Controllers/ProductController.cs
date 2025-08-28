bc492353d7e09619c324dfec0fb8ff2660b6a491
thduc2005 <thduc2005@gmail.com>
2025-08-28 18:00:42 +0700

    first complete

---
+++ Controllers/ProductController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BaiTH1.Controllers
+{
+    public class ProductController : Controller
+    {
+        public IActionResult List()
+        {
+            return View();
+        }
+
+        public IActionResult Details()
+        {
+            return View();
+        }
+    }
+}
---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace BaiTH1.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
